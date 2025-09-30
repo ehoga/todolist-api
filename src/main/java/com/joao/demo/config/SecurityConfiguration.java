e9ec7afbaba94fa78e8511d2c305a9dbb478f604
gabuilders <gabriel.arthur@platformbuilders.io>
2025-09-30 18:34:41 -0300

    Adicionando permissão para o front-end consumir a api

--- src/main/java/com/joao/demo/config/SecurityConfiguration.java
+++ src/main/java/com/joao/demo/config/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://3.17.62.217", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
